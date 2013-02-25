fd2d0f69d7bfb7bbd703c4602ac000da795db264
Jesse Snyder <jsnyder@wesleyan.edu>
2013-02-24 21:44:22 -0800

    renamed app, so update settings.py

--- karawitan_project/settings.py
+++ karawitan_project/settings.py
@@ -124,7 +124,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'south',
-    'recordings',
+    'klenengan',
 )
 
 # A sample logging configuration. The only tangible logging
